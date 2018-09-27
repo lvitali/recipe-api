942c7bcd996f6014795adf700af48be27a4fcf7f
Leonardo <lenardovitalik@gmail.com>
2018-09-27 17:43:33 -0300

    Adicionar método para buscar receitas na API do recipepuppy
    Utilizando os ingredientes informados as receitas serão pesquisadas na API,
    caso a busca não retorne resultados um erro será lançado.

--- src/infrastructure/services/recipeService.js
+++ src/infrastructure/services/recipeService.js
@@ -2,15 +2,32 @@ import createHttpClient from '../support/httpClientFactory'
 
 const httpClient = createHttpClient('http://www.recipepuppy.com/api/')
 
+const throwErrorForEmptyResponse = (ingredients) => {
+  const error = new Error(`No recipes found with ingredients ${ingredients}`)
+  error.status = 404
+  throw error
+}
+
+const searchRecipes = async (ingredients) => {
+  const response = await httpClient.get(`?i=${ingredients}`)
+
+  const emptyResponse = response.data.results.length === 0
+
+  if (emptyResponse) {
+    throwErrorForEmptyResponse(ingredients)
+  }
+
+  return response.data.results
+}
+
 const ping = async () => {
   const response = await httpClient.get()
 
   if (response.status === 503) {
-    const error = new Error('Serviço de receitas indisponível')
+    const error = new Error('Recipe service is unavailable')
     error.status = 503
     throw error
   }
 }
 
-export { ping }
-
+export { searchRecipes, ping }
