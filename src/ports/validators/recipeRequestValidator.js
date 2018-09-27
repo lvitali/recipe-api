310d95164f2eb7c791fc86c9c3deaf478a3f955b
Leonardo <lenardovitalik@gmail.com>
2018-09-27 17:46:19 -0300

    Criar validador para a request do endpoint /recipes
    Essa função valida a query string, que é entrada de dados do port rest.

---
+++ src/ports/validators/recipeRequestValidator.js
@@ -0,0 +1,37 @@
+const checkQsParameters = (request) => {
+  if (!request.query) {
+    throw new Error('The query string must not be empty')
+  }
+
+  if (!('i' in request.query) || request.query === null) {
+    throw new Error('The i parameter is required in query string e cannot be null')
+  }
+}
+
+const checkQsPattern = (request) => {
+  const pattern = /([^0-9]+,){2}/gy
+
+  const isValid = pattern.test(request.query.i)
+
+  if (!isValid) {
+    throw new Error('Ingredients must be informed in the following pattern eggs,onion')
+  }
+}
+
+const validateParametersSize = (value) => {
+  const valuesCount = value.split(',').length
+
+  if (valuesCount > 3) {
+    throw new Error('No máximo 3 ingredientes devem ser informados')
+  }
+}
+
+const validate = async (ctx, next) => {
+  checkQsParameters(ctx)
+  checkQsPattern(ctx)
+  validateParametersSize(ctx.query.i)
+
+  await next()
+}
+
+export { validate }
