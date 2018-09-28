f2f355d0845b2f1a243979e91cca7408c24d1a27
Leonardo <lenardovitalik@gmail.com>
2018-09-28 10:55:23 -0300

    Atualizar gifService para utilizar a função que recupera as variáveis de ambiente a partir de sua chave

--- src/infrastructure/services/gifService.js
+++ src/infrastructure/services/gifService.js
@@ -1,10 +1,11 @@
 const createHttpClient = require('../support/httpClientFactory')
+const env = require('../support/env')
 
 const httpClient = createHttpClient('https://api.giphy.com/v1')
 
 const searchGif = async (title) => {
   try {
-    const apiKey = process.env.GIPHY_API_KEY
+    const apiKey = env('GIPHY_API_KEY')
     const endpoint = `/gifs/search?api_key=${apiKey}&q=${title}&limit=1&lang=en`
     const response = await httpClient.get(endpoint)
 
